af27b25f5239ebba5572db71641dda660b6a07dd
Markus Stenberg <markus.stenberg@iki.fi>
2026-07-19 09:57:15 +0200

    Expose Swift runtime from repository root

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "ProprDBSwiftRuntime",
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "ProprDBSwiftRuntime",
+            targets: ["ProprDBSwiftRuntime"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-log.git",
+            from: "1.6.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-protobuf.git",
+            from: "1.31.0"
+        ),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CSQLite",
+            path: "swift/ProprDBSwiftRuntime/Sources/CSQLite"
+        ),
+        .target(
+            name: "ProprDBSwiftRuntime",
+            dependencies: [
+                "CSQLite",
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ],
+            path: "swift/ProprDBSwiftRuntime/Sources/ProprDBSwiftRuntime",
+            swiftSettings: [
+                .swiftLanguageMode(.v6),
+            ]
+        ),
+    ]
+)
